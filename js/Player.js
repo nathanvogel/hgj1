75510889c0678695d54a2dd1624cfb7b8f0dafb6
Nathan Vogel <nathan@nathanvogel.com>
2018-02-10 21:26:27 +0100

    woeifj

---
+++ js/Player.js
@@ -0,0 +1,31 @@
+/* global paper Size Shape Point Path view project */
+/* global App Player MyClass Road myApp levels */
+
+window.Player = function(levelName) {
+  this.myVar = 0;
+  console.log("init Player");
+};
+
+Player.prototype = {
+  myFunction: function(levelData) {},
+
+  onKeyUp: function(event) {
+    switch (event.key) {
+      case "1":
+        break;
+      default:
+    }
+  },
+
+  onKeyDown: function(event) {
+    switch (event.key) {
+      case "1":
+        break;
+      default:
+    }
+  }
+};
+
+function onKeyDown(event) {
+  console.log("oiwjef");
+}
