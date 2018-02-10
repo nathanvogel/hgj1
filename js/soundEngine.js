bc1ebed05638a096c3b2b10074eda0f6bc3b88fc
lsdebianchi <l.s.contatto@gmail.com>
2018-02-10 21:13:38 +0100

    index

--- js/soundEngine.js
+++ js/soundEngine.js
@@ -2,6 +2,20 @@
 SOUNDS = {};
 
 
-function newSound( name ){
 
-};
+
+function newSound(name, path){
+
+  var sound = SOUNDS[name];
+  sound = document.createElement("audio");
+  sound.src = path;
+  sound.setAttribute("preload", "auto");
+  sound.setAttribute("controls", "none");
+  sound.style.display = "none";
+  document.body.appendChild(sound);
+
+}
+
+newSound("click", "../sounds/click.mp3");
+
+SOUNDS.click.play();
